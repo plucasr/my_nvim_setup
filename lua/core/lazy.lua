3d91ef09e36e2d910ad5785cc4276acebc904720
Lucas Pedroso <webdevlucas@gmail.com>
2025-04-21 10:35:13 -0300

    LSP and DAP setup

--- lua/core/lazy.lua
+++ lua/core/lazy.lua
@@ -13,101 +13,23 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 return require('lazy').setup({
-	-- No need for packer.nvim (lazy self-manages)
-
-	-- Telescope (fuzzy finder)
+	-- Core LSP Management (Following Documentation Style)
 	{
-		'nvim-telescope/telescope.nvim',
-		tag = '0.1.8',  -- or branch = '0.1.x'
-		dependencies = { 'nvim-lua/plenary.nvim' },
-	},
-
-	-- Rose Pine colorscheme (load immediately)
-	{
-		'rose-pine/neovim',
-		name = 'rose-pine',  -- 'as=' becomes 'name='
-		lazy = false,        -- Load at startup
-		priority = 1000,     -- High priority for colorschemes
+		"williamboman/mason.nvim",
 		config = function()
-			vim.cmd.colorscheme('rose-pine')
+			require("mason").setup()
 		end,
 	},
-
-	-- Treesitter (syntax highlighting)
 	{
-		'nvim-treesitter/nvim-treesitter',
-		build = ':TSUpdate',  -- 'run=' becomes 'build='
-	},
-
-	-- Treesitter playground
-	{ 'nvim-treesitter/playground' },
-
-	-- Harpoon (navigation)
-	{ 'theprimeagen/harpoon' },
-
-	-- Fugitive (Git)
-	{ 'tpope/vim-fugitive' },
-
-	-- Undotree
-	{ 'mbbill/undotree' },
-
-	-- Icons (loaded immediately - fixes some UI issues)
-	{
-		'nvim-tree/nvim-web-devicons',
-		lazy = false,  -- Load immediately (avoids flash of missing icons)
-	},
-
-	-- Gitsigns (Git status)
-	{ 'lewis6991/gitsigns.nvim' },
-
-	-- Barbar (buffer tabs)
-	{ 'romgrk/barbar.nvim' },
-	{ 'VonHeikemen/lsp-zero.nvim', requires = {
-		{'neovim/nvim-lspconfig'},
-		{'williamboman/mason.nvim'},
-		{'williamboman/mason-lspconfig.nvim'},
-		{'hrsh7th/nvim-cmp'},
-		{'hrsh7th/cmp-buffer'},
-		{'hrsh7th/cmp-path'},
-		{'saadparwaiz1/cmp_luasnip'},
-		{'hrsh7th/cmp-nvim-lsp'},
-		{'hrsh7th/cmp-nvim-lua'},
-		{'L3MON4D3/LuaSnip'},
-	}},
-	{
-		"williamboman/mason.nvim"
-	},
-	-- In your lazy.nvim setup (plugins.lua or equivalent)
-	-- In your lazy.nvim setup (plugins.lua or equivalent)
-	{
-		"nvim-tree/nvim-tree.lua",
-		lazy = false, -- Important for directory opens
+		"williamboman/mason-lspconfig.nvim",
 		config = function()
-			require("nvim-tree").setup({
-				-- your existing config
+			require("mason-lspconfig").setup({
+				ensure_installed = { "typescript-language-server", "eslint", "python-lsp-server", "ruby-lsp", "omnisharp" }, -- Add your desired servers
+				automatic_installation = true, -- Automatically install missing servers
 			})
-
-			-- Auto-open when directory is detected
-			vim.api.nvim_create_autocmd({ "VimEnter" }, {
-				callback = function(data)
-					-- Only open if argument is a directory
-					local directory = vim.fn.isdirectory(data.file) == 1
-					if not directory then return end
-
-					-- Change to the directory
-					vim.cmd.cd(data.file)
-
-					-- Open nvim-tree
-					require("nvim-tree.api").tree.open({
-						current_window = false, -- Open in sidebar, not full window
-						focus = true,
-						find_file = true
-					})
-				end
-			})
-		end
+		end,
+		dependencies = { "williamboman/mason.nvim" },
 	},
-	{"numToStr/FTerm.nvim"},
 	{
 		"neovim/nvim-lspconfig",
 		dependencies = {
@@ -115,49 +37,64 @@ return require('lazy').setup({
 			"williamboman/mason-lspconfig.nvim",
 		},
 		config = function()
-			require("mason").setup()
-			require("mason-lspconfig").setup({
-				ensure_installed = { "ts_ls", "eslint" }
-			})
-
 			local lspconfig = require("lspconfig")
 			local util = require("lspconfig.util")
 
-			-- TypeScript (supports monorepos)
-			lspconfig.ts_ls.setup{}
-
-
-			-- ESLint
-			lspconfig.eslint.setup({
-				root_dir = util.root_pattern(".eslintrc.js", ".eslintrc.json", ".eslintrc.cjs"),
-				settings = {
-					workingDirectory = { mode = "auto" }
-				},
-				on_attach = function(client, bufnr)
-					vim.api.nvim_create_autocmd("BufWritePre", {
-						buffer = bufnr,
-						command = "EslintFixAll",
+			require("mason-lspconfig").setup_handlers {
+				function (server_name) -- Default handler
+					require("lspconfig")[server_name].setup {}
+				end,
+				["typescript-language-server"] = function ()
+					lspconfig.tsserver.setup {}
+				end,
+				["eslint"] = function ()
+					lspconfig.eslint.setup({
+						root_dir = util.root_pattern(".eslintrc.js", ".eslintrc.json", ".eslintrc.cjs"),
+						settings = {
+							workingDirectory = { mode = "auto" }
+						},
+						on_attach = function(client, bufnr)
+							vim.api.nvim_create_autocmd("BufWritePre", {
+								buffer = bufnr,
+								command = "EslintFixAll",
+							})
+						end
 					})
-				end
-			})
-		end
-	},
-	{
-		'williamboman/mason-lspconfig.nvim',
-		config = function()
-			require('mason-lspconfig').setup({
-				ensure_installed = { 'ts_ls' }
-			})
+				end,
+				["python-lsp-server"] = function()
+					lspconfig.pyright.setup {} -- Or other Python LSP server
+				end,
+				["ruby-lsp"] = function()
+					lspconfig.ruby_ls.setup {}
+				end,
+				["omnisharp"] = function()
+					lspconfig.omnisharp.setup {}
+				end,
+				-- Add more server-specific handlers here
+			}
 		end,
-		dependencies = { 'williamboman/mason.nvim' }
 	},
+
+
+	-- Your other plugins (keep these)
+	{ 'nvim-telescope/telescope.nvim', tag = '0.1.8', dependencies = { 'nvim-lua/plenary.nvim' } },
+	{ 'rose-pine/neovim', name = 'rose-pine', lazy = false, priority = 1000, config = function() vim.cmd.colorscheme('rose-pine') end },
+	{ 'nvim-treesitter/nvim-treesitter', build = ':TSUpdate' },
+	{ 'nvim-treesitter/playground' },
+	{ 'theprimeagen/harpoon' },
+	{ 'tpope/vim-fugitive' },
+	{ 'mbbill/undotree' },
+	{ 'nvim-tree/nvim-web-devicons', lazy = false },
+	{ 'lewis6991/gitsigns.nvim' },
+	{ 'romgrk/barbar.nvim' },
+	{ "nvim-tree/nvim-tree.lua", lazy = false, config = function() require("nvim-tree").setup({}) end },
+	{"numToStr/FTerm.nvim"},
 	{
 		"mfussenegger/nvim-dap",
 		config = function()
 			local dap = require("dap")
-			local util = require('lspconfig.util')  -- Add this line
+			local util = require('lspconfig.util')
 
-			-- Node.js/TypeScript Debugger
 			dap.adapters.node = {
 				type = "server",
 				host = "127.0.0.1",
@@ -172,14 +109,13 @@ return require('lazy').setup({
 				},
 			}
 
-			-- Debug configurations matching your launch.json
 			dap.configurations.typescript = {
 				{
 					name = "Debug Server",
 					type = "node",
 					request = "launch",
 					cwd = "${workspaceFolder}/packages/web",
-					runtimeExecutable = "yarn",
+					runtimeExecutable = "pnpm",
 					runtimeArgs = { "dev" },
 					console = "integratedTerminal",
 					sourceMaps = true,
@@ -204,36 +140,14 @@ return require('lazy').setup({
 	{
 		"mxsdev/nvim-dap-vscode-js",
 		dependencies = { "mfussenegger/nvim-dap" },
-		config = function()
-			require("dap-vscode-js").setup({
-				debugger_path = vim.fn.stdpath("data") .. "/mason/packages/js-debug-adapter",
-				adapters = { "pwa-node", "pwa-chrome" }
-			})
-		end
+		config = function() require("dap-vscode-js").setup({ adapters = { "pwa-node", "pwa-chrome" } }) end
 	},
 	{
 		'jose-elias-alvarez/null-ls.nvim',
 		dependencies = { 'nvim-lua/plenary.nvim' },
-		config = function()
-			local null_ls = require('null-ls')
-			local util = require('lspconfig.util')  -- This line must be here
-
-			null_ls.setup({
-				sources = {
-					null_ls.builtins.formatting.prettier.with({
-						prefer_local = 'node_modules/.bin'
-					}),
-				},
-				on_attach = function(client, bufnr)
-					vim.api.nvim_create_autocmd('BufWritePre', {
-						buffer = bufnr,
-						callback = function()
-							vim.lsp.buf.format({ async = false })
-						end,
-					})
-				end,
-			})
-		end
+		config = function() local null_ls = require('null-ls'); null_ls.setup({ sources = { null_ls.builtins.formatting.prettier.with({ prefer_local = 'node_modules/.bin' }) }, on_attach = function(client, bufnr) vim.api.nvim_create_autocmd('BufWritePre', { buffer = bufnr, callback = function() vim.lsp.buf.format({ async = false }) end }) end }) end
 	},
+
+
 })
 
