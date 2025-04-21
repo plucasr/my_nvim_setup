b0bb811e916df91450791aa6f63a80957d47cebb
Lucas Pedroso <webdevlucas@gmail.com>
2025-04-21 14:11:37 -0300

    killer version

--- lua/core/lazy.lua
+++ lua/core/lazy.lua
@@ -45,7 +45,12 @@ return require('lazy').setup({
 					require("lspconfig")[server_name].setup {}
 				end,
 				["typescript-language-server"] = function ()
-					lspconfig.tsserver.setup {}
+					lspconfig.tsserver.setup {
+						on_attach = function(client, bufnr)
+							vim.keymap.set('n', 'gd', vim.lsp.buf.definition, { buffer = bufnr, desc = 'Go to Definition' })
+							-- You can add other key mappings here for LSP actions
+						end,
+					}
 				end,
 				["eslint"] = function ()
 					lspconfig.eslint.setup({
@@ -144,25 +149,73 @@ return require('lazy').setup({
 	},
 	{
 		'jose-elias-alvarez/null-ls.nvim',
-		dependencies = { 'nvim-lua/plenary.nvim' },
-		config = function() local null_ls = require('null-ls'); null_ls.setup({ sources = { null_ls.builtins.formatting.prettier.with({ prefer_local = 'node_modules/.bin' }) }, on_attach = function(client, bufnr) vim.api.nvim_create_autocmd('BufWritePre', { buffer = bufnr, callback = function() vim.lsp.buf.format({ async = false }) end }) end }) end
-	},
-	{ "nvim-neotest/nvim-nio" }, -- Ensure nvim-nio is listed
+		dependencies = { 
+			'nvim-lua/plenary.nvim' 
+		},
+		config = function() 
+			local null_ls = require('null-ls'); 
+			null_ls.setup({ 
+				sources = { 
+					null_ls.builtins.formatting.prettier.with({ prefer_local = 'node_modules/.bin' }) },
+					on_attach = function(client, bufnr) 
+						vim.api.nvim_create_autocmd('BufWritePre', { buffer = bufnr, callback = function() vim.lsp.buf.format({ async = false }) end }) end 
+					}) end
+				},
+				{ "nvim-neotest/nvim-nio" }, -- Ensure nvim-nio is listed
 
-	{
-		"rcarriga/nvim-dap-ui",
-		dependencies = { "mfussenegger/nvim-dap", "nvim-neotest/nvim-nio" },
-		config = function()
-			local nio_status, nio = pcall(require, "nio")
-			if not nio_status then
-				vim.notify_error("nvim-nio could not be loaded. nvim-dap-ui might not work correctly.")
-				return
-			end
-			require("dapui").setup()
-		end,
-	},
-	{ "windwp/nvim-autopairs", config = true },
+				{
+					"rcarriga/nvim-dap-ui",
+					dependencies = { "mfussenegger/nvim-dap", "nvim-neotest/nvim-nio" },
+					config = function()
+						local nio_status, nio = pcall(require, "nio")
+						if not nio_status then
+							vim.notify_error("nvim-nio could not be loaded. nvim-dap-ui might not work correctly.")
+							return
+						end
+						require("dapui").setup()
+					end,
+				},
+				{ "windwp/nvim-autopairs", config = true },
+				-- Completion (nvim-cmp)
+				{ 'hrsh7th/nvim-cmp', config = function()
+					local cmp = require('cmp')
+					cmp.setup({
+						mapping = cmp.mapping.preset.insert({
+							['<C-space>'] = cmp.mapping.complete(),
+							['<CR>'] = cmp.mapping.confirm({ select = true }),
+							['<C-p>'] = cmp.mapping.select_prev_item(),
+							['<C-n>'] = cmp.mapping.select_next_item(),
+						}),
+						sources = {
+							{ name = 'nvim_lsp' },
+							{ name = 'luasnip' },
+							{ name = 'buffer' },
+							{ name = 'path' },
+						},
+						-- You can add more configuration options here
+					})
+				end },
+				{ 'hrsh7th/cmp-buffer' },      -- Completion from open buffers
+				{ 'hrsh7th/cmp-path' },        -- Completion for file paths
+				{ 'saadparwaiz1/cmp_luasnip' }, -- Completion from snippets (requires a snippet engine)
+				{ 'hrsh7th/cmp-nvim-lsp' },   -- Completion from LSP servers
+				{ 'hrsh7th/cmp-nvim-lua' },   -- Completion for Lua code
 
+				-- Snippets (if you don't have one already, LuaSnip is good)
+				{ 'L3MON4D3/LuaSnip' },
+				{ 'rafamadriz/friendly-snippets' }, -- Optional: A collection of snippets
+				{
+					"f-person/git-blame.nvim",
+					event = "VeryLazy",
+					opts = {
+						-- your configuration comes here
+						-- for example
+						enabled = true,  -- if you want to enable the plugin
+						message_template = " <author> • <date> • <summary> • <<sha>>", -- template for the blame message, check the Message template section for more options
+						date_format = "%m-%d-%Y %H:%M:%S", -- template for the date, check Date format section for more options
+						virtual_text_column = 1,  -- virtual text start column, check Start virtual text at column section for more options
+					},
 
-})
+				}
+			})
 
